fcf9d925dce453ddea278d4ebb1b61c6db2a9f35
Keks <keks@htmlacademy.ru>
2023-03-21 19:59:17 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/data.js
@@ -0,0 +1,67 @@
+import {getRandomArrayElement, createIdGenerator, shuffleArray, createRandomId} from './util.js';
+
+const DESCRIPTIONS = [
+  'Лучшее фото в моей жизни!',
+  'Это прекрасно!',
+  'Лепота-то какая!',
+  'Мне нравится',
+  'Думаю, красиво',
+  'Очень хорошо сегодня',
+  'Чудесный был день!',
+];
+
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const NAMES = [
+  'Игорь',
+  'Ян',
+  'Роберт',
+  'Юрий',
+  'Аркадий',
+  'Лена',
+  'Татьяна',
+  'Илья',
+  'Фёдор',
+];
+
+const SIMILAR_PHOTO_COUNT = 25;
+const SIMILAR_COMMENT_COUNT = 3;
+const MIN_LIKE = 15;
+const MAX_LIKE = 200;
+const MIN_AVATAR = 1;
+const MAX_AVATAR = 6;
+
+const likeValues = shuffleArray(Array.from({length: MAX_LIKE - MIN_LIKE + 1}, (_, index) => index + MIN_LIKE));
+const avatarValues = shuffleArray(Array.from({length: MAX_AVATAR - MIN_AVATAR + 1}, (_, index) => index + MIN_AVATAR));
+
+const generateCommentId = createIdGenerator();
+const generatePhotoId = createIdGenerator();
+const generateImageId = createIdGenerator();
+const generateLikeValue = createRandomId(likeValues);
+const generateAvatarValue = createRandomId(avatarValues);
+
+const createComment = () => ({
+  id: generateCommentId(),
+  avatar: `img/avatar-${generateAvatarValue()}.svg`,
+  message: getRandomArrayElement(MESSAGES),
+  name: getRandomArrayElement(NAMES),
+});
+
+const createPhoto = () => ({
+  id: generatePhotoId(),
+  url: `photos/${generateImageId()}.jpg`,
+  description: getRandomArrayElement(DESCRIPTIONS),
+  likes: generateLikeValue(),
+  comments: Array.from({ length: SIMILAR_COMMENT_COUNT }, createComment),
+});
+
+const createPhotos = () => Array.from({ length: SIMILAR_PHOTO_COUNT }, createPhoto);
+
+export {createPhotos};
---
+++ 5/js/thumbnail.js
@@ -0,0 +1,20 @@
+import {createPhotos} from './data.js';
+
+const picturesContainer = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+const thumbnails = createPhotos();
+
+const similarThumbnail = document.createDocumentFragment();
+
+thumbnails.forEach(({url, comments, likes}) => {
+  const thumbnailElement = pictureTemplate.cloneNode(true);
+  thumbnailElement.querySelector('.picture__img').src = url;
+  thumbnailElement.querySelector('.picture__comments').textContent = comments.length;
+  thumbnailElement.querySelector('.picture__likes').textContent = likes;
+  similarThumbnail.appendChild(thumbnailElement);
+});
+
+picturesContainer.appendChild(similarThumbnail);
