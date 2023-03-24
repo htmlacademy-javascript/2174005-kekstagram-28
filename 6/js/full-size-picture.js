34b6a64b9fe79016fa4f2f292675150b231dc1ba
Keks <keks@htmlacademy.ru>
2023-03-24 09:52:32 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/full-size-picture.js
@@ -0,0 +1,64 @@
+import {isEscapeKey} from './util.js';
+
+const body = document.querySelector('body');
+const bigPictureContainer = document.querySelector('.big-picture');
+const bigImage = bigPictureContainer.querySelector('.big-picture__img');
+const bigImageLikesCount = bigPictureContainer.querySelector('.likes-count');
+const bigImageCaption = bigPictureContainer.querySelector('.social__caption');
+const bigImageCancel = bigPictureContainer.querySelector('.big-picture__cancel');
+
+
+const commentsContainer = bigPictureContainer.querySelector('.social__comments');
+const commentItem = commentsContainer.querySelector('.social__comment');
+const commentCount = bigPictureContainer.querySelector('.comments-count');
+const commentsCount = bigPictureContainer.querySelector('.social__comment-count');
+const commentsLoader = bigPictureContainer.querySelector('.comments-loader');
+
+const onDocumentKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeUserModal();
+  }
+};
+
+function openUserModal () {
+  bigPictureContainer.classList.remove('hidden');
+  body.classList.add('modal-open');
+  document.addEventListener('keydown', onDocumentKeydown);
+  commentsCount.classList.add('hidden');
+  commentsLoader.classList.add('hidden');
+}
+
+function closeUserModal () {
+  bigPictureContainer.classList.add('hidden');
+  body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocumentKeydown);
+  commentsCount.classList.remove('hidden');
+  commentsLoader.classList.remove('hidden');
+}
+
+const renderComments = (commentsArray) => {
+  commentsArray.forEach(({avatar, name, message}) => {
+    const newComment = commentItem.cloneNode(true);
+    newComment.querySelector('.social__picture').src = avatar;
+    newComment.querySelector('.social__picture').alt = name;
+    newComment.querySelector('.social__text').textContent = message;
+    commentsContainer.append(newComment);
+  });
+};
+
+const showBigPicture = (url, likes, comments, description) => {
+  openUserModal();
+  bigImage.querySelector('img').src = url;
+  bigImageLikesCount.textContent = likes;
+  commentCount.textContent = comments.length;
+  bigImageCaption.textContent = description;
+  commentsContainer.innerHTML = '';
+  renderComments(comments);
+};
+
+bigImageCancel.addEventListener('click', () =>
+  closeUserModal()
+);
+
+export {showBigPicture};
---
+++ 6/js/thumbnail.js
@@ -0,0 +1,23 @@
+import {createPhotos} from './data.js';
+import {showBigPicture} from './full-size-picture.js';
+
+const thumbnailsContainer = document.querySelector('.pictures');
+const thumbnailTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+const renderThumbnails = createPhotos();
+const similarThumbnails = document.createDocumentFragment();
+
+renderThumbnails.forEach(({url, comments, likes, description}) => {
+  const thumbnailElement = thumbnailTemplate.cloneNode(true);
+  thumbnailElement.querySelector('.picture__img').src = url;
+  thumbnailElement.querySelector('.picture__comments').textContent = comments.length;
+  thumbnailElement.querySelector('.picture__likes').textContent = likes;
+  thumbnailElement.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    showBigPicture(url, likes, comments, description);
+  });
+  similarThumbnails.append(thumbnailElement);
+});
+
+thumbnailsContainer.append(similarThumbnails);
