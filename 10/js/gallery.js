36738d0171410b807450d6c9eefe3fdf2b9b80d9
Keks <keks@htmlacademy.ru>
2023-04-04 21:36:22 +0300

    :heavy_check_mark: Сборка #10

--- 10/js/gallery.js
+++ 10/js/gallery.js
@@ -1,11 +1,7 @@
 import { renderBigPicture } from './full-size-picture.js';
-import { showAlert } from './util.js';
 import { getData } from './api.js';
 
 getData()
   .then((data) => {
     renderBigPicture(data);
-  })
-  .catch((err) => {
-    showAlert(err.message);
   });
